041ecc36ca4a2f04794621e27a593d9c0fc118d5
S-khudairi <Samkhudairi2002@gmail.com>
2025-09-04 16:24:10 -0500

    Start of site

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("clickBtn");
+    button.addEventListener("click", () => {
+      alert("Hello! You clicked the button 🚀");
+    });
+  });
+  
